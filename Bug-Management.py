690f4398334eaa87ce216459e90ccbee4e04874d
Norx-Nishidh47 <156525982+Norx-Nishidh47@users.noreply.github.com>
2024-09-04 01:08:25 +0530

    Added new functionalities and solved some bugs

--- Bug-Management.py
+++ Bug-Management.py
@@ -3,6 +3,8 @@ import modules as easy
 import os
 import time
 
+# actualsts = {1:'Intest', 2:'Open', 3:'Close', 4:'Reopen', 5:'Inprogress'}
+
 # Connect to MySQL
 db = mysql.connect(host="localhost", user="root", passwd="nishidh@123", database="bug_tracking")
 cur = db.cursor()  #creating cursor
@@ -13,10 +15,35 @@ def credentials():  #taking credentials
     global password
     email, password = str(input('Enter the email address : ')), str(input("Enter your password"))
 
-def bugreport():  #edit code status
-    actualsts = {1:'Intest', 2:'Open', 3:'Close', 4:'Reopen', 5:'Inprogress'}
+def codestatus():
+    bugID = input('Enter the Id of your respective Bug : ')
+    print({1:'Intest', 2:'Open', 3:'Close', 4:'Reopen', 5:'Inprogress'})
+    choice = int(input('Enter your choice (1/2/3/4/5) => '))
+    easy.update('buginfo', 'bugstatus', choice, f"bugID = '{bugID}'")
+    os.system('clear')
+    print('Updating ....'), time.sleep(3)
+    print('Updated Successfully')
+
+def addbug():
+    id = easy.bugID()
+    bugds = str(input('Enter the bug Description : '))
+    severity = str(input('Enter the severity of the bug : '))
+    bugstatus = str(input("{1:'Intest', 2:'Open', 3:'Close', 4:'Reopen', 5:'Inprogress'} Enter (1/2/3/4/5): " ))
+    reqdays = int(input('Enter the no. of days required : '))
+    opendt = str(input('Enter the date of Opening : '))#2020-03-23
+    closingdt = str(input('Enter the date of Closing (expected)'))
+    priority = int(input("PRIORITY => {1:'high', 2:'Medium', 3:'low'} Enter (1/2/3): "))
+    assign_to = str(input('Enter the name of the assigne: '))
+    assign_by = str(input('Enter the name of bug assigner: '))
+    cur.execute(f"insert into buginfo values {(id,bugstatus, bugds, severity, reqdays, opendt, closingdt, priority)}"), db.commit()
+    cur.execute(f"insert into assign (assign_to,assigned_by,bugID) values {(assign_to,assign_by,id)}"), db.commit()
+    print('------------------ BUG SUCCESSFULLY UPDATED ------------------'), time.sleep(4)
+    work()
+
+def bugreport():  #edit code status  
     global id 
     id = easy.bugID()
+    actualsts = {1:'Intest', 2:'Open', 3:'Close', 4:'Reopen', 5:'Inprogress'}
     print('Current Code Status => ')
     """ CAN  USE TABULATE LIBRARY BUT ALREADY USED TOO MANY ${NOT TO TAKE BURDEN}"""
     status = easy.select('buginfo', '*', 1, f"bugID = '{id}'") 
@@ -24,10 +51,16 @@ def bugreport():  #edit code status
 def assignes():#switch assignes
     id = easy.bugID()
     if (id != None):
-        cur.execute(f"select assign_to from assign where bugID = '{id}'")
-        print('fetching Assignes ... ')
+        cur.execute(f"select assign_to,bugID,assigned_by from assign where bugID = '{id}'")
+        print('fetching Assignes ... '), time.sleep(3)
         os.system('clear')
-        print(f'Current Assignee => {cur.fetchone()}')
+        # print(f'Current Assignee => {cur.fetchone()}' if cur.fetchone!= None else print('-------------- BugID not found --------------'), time.sleep(4) ,assignes())
+        bugdetails = cur.fetchone()
+        if bugdetails!= None:
+            print(f'Current Assignee => {bugdetails}')
+        else:
+            print('-------------- BugID not found --------------')
+            assignes()
     elif (id == None):
         id = input('Enter the bugID')# if the bugID is not present
         cur.execute(f"select assign_to from assign where bugID = '{id}'")
@@ -39,10 +72,16 @@ def assignes():#switch assignes
         details = easy.select('userinfo', 'username,email', 90, f"username like '{names}%'")
         os.system('clear')
         print('( username , Email )\n', *(i for i in details), sep='\n')
-    else:names = str(input('Enter the name to find the EmailID : ' ))
-    cur.execute(f"select username from userinfo where username like '{names}%'")
-    if (cur.fetchall() != []):
-        newassign = str(input('Enter the new assignee : '))
+        time.sleep(4)
+    elif (choice == 2):
+        email = str(input('Enter the EmailID of the person : '))
+        easy.update('assign', 'assign_to', email, f"bugID = '{id}'")
+    else:
+        os.system('clear'),print('------------------------- wrong choice selected -------------------------')
+        time.sleep(4)
+        assignes()
+    if (details != []):
+        newassign = str(input('Enter the new assignee EmailId : '))
         os.system('clear')
         print('Updating Assignee ... ')
         time.sleep(4)
@@ -50,9 +89,11 @@ def assignes():#switch assignes
         cur.execute(f"update assign set assign_to = '{newassign}' where bugID = '{id}'")
         print('Assignee updated successfully')
         time.sleep(2)
+    else:
+        print("--------------- Enter name properly ; This name didn't exist in our data ---------------")
+        assignes()
     db.commit()
-    os.system('clear')
-    print("The memeber doesn't exist"), time.sleep(4)
+    os.system('clear'), time.sleep(3)
     if input('Press Enter to continue ...') == '': os.system('clear'), work()
 
 def allbugs():
@@ -65,17 +106,22 @@ def allbugs():
     print("PRIORITY => {1:'high', 2:'Medium', 3:'low'}")
 
 def work():
-    print('--------------------------------- What you want to do ---------------------------------\n {1} => Edit code status \n {2} => Switch Assignes \n {3} => Bug Report \n {4} => See All bugs \n {5} => logout / login ')
+    print('--------------------------------- What you want to do ---------------------------------\n {1} => Edit code status \n {2} => Switch Assignes \n {3} => Bug Report \n {4} => See All bugs \n {5} => Add bugs \n {6} => logout / login ')
     newchoice = int(input('Enter your choice'))
     if newchoice == 1:
         codestatus()
+        work()
     elif newchoice == 2:
         assignes()
     elif newchoice == 3:
         bugreport()
+        work()
     elif newchoice == 4:
         allbugs()
+        work()
     elif newchoice == 5:
+        addbug()
+    elif newchoice == 6:
         user_login()
 
 def user_login():  #login/Signin
